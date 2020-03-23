beba70168c54df8a96397c4ed0d38b957275ee9f
Akihiko Nakamura <anakamuro@earthlink.net>
2020-03-23 14:02:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,51 @@ import React from "react";
 import BooksApp from "./BooksApp";
 import Search from "./Search";
 import "./App.css";
+import * as BooksAPI from "./BooksAPI";
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={BooksApp} />
-        <Route path="/search" component={Search} />
-      </Switch>
-    </Router>
-  );
-};
+class App extends React.Component {
+  state = {
+    book: [],
+    error: false
+  };
 
+  moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).catch(err => {
+      console.log(err);
+      this.setState({ error: true });
+    });
+    if (shelf === "none") {
+      this.setState(prevState => ({
+        books: prevState.books.filter(b => b.id !== book.id)
+      }));
+    } else {
+      book.shelf = shelf;
+      this.setState(prevState => ({
+        books: prevState.books.filter(b => b.id !== book.id).concat(book)
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <BooksApp book={this.state.book} onMove={this.moveShelf} />
+            )}
+          />
+          <Route
+            path="/search"
+            render={() => (
+              <Search book={this.state.book} onMove={this.moveShelf} />
+            )}
+          />
+        </Switch>
+      </Router>
+    );
+  }
+}
 export default App;
